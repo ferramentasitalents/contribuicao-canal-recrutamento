acc3bbfa5a188a3dc6c1ff68a41691d84905b534
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-15 17:06:08 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateContribution() {
+  const numAplicantesCanal = parseFloat(document.getElementById("num-aplicantes-canal").value);
+  const numAplicantesTotal = parseFloat(document.getElementById("num-aplicantes-total").value);
+  const contribution = (numAplicantesCanal / numAplicantesTotal) * 100;
+  document.getElementById("result").innerHTML = `Contribuição do Canal de Recrutamento: ${contribution.toFixed(2)}%`;
+}
 
